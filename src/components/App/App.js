52a846978ba3491b8d3f17ab8eb71b1b6bf2c286
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-02-17 21:56:13 +0100

    add lazy import for pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,36 @@
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Navigation } from 'components/Navigation/Navigation';
-import { HomePage } from 'pages/HomePage/HomePage';
-import { MoviesPage } from 'pages/MoviesPage/MoviesPage';
-import { MoviesDetaile } from 'pages/MoviesDetailesPage/MoviesDetailesPage';
-import { Cast } from 'pages/Cast/Cast';
-import { Reviews } from 'pages/Reviews/Reviews';
+// import { Navigation } from 'components/Navigation/Navigation';
+// import { HomePage } from 'pages/HomePage/HomePage';
+// import { MoviesPage } from 'pages/MoviesPage/MoviesPage';
+// import { MoviesDetaile } from 'pages/MoviesDetailesPage/MoviesDetailesPage';
+// import { Cast } from 'pages/Cast/Cast';
+// import { Reviews } from 'pages/Reviews/Reviews';
+const Navigation = lazy(() => import('components/Navigation/Navigation'));
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('pages/MoviesPage/MoviesPage'));
+const MoviesDetaile = lazy(() =>
+  import('pages/MoviesDetailesPage/MoviesDetailesPage')
+);
+const Cast = lazy(() => import('pages/Cast/Cast'));
+const Reviews = lazy(() => import('pages/Reviews/Reviews'));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MoviesDetaile />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
+      <Suspense fallback="">
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/movies/:movieId" element={<MoviesDetaile />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 };
