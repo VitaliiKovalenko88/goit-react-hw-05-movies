9b629e72eab94e76be4add90a54432ee8c44c4be
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-12-08 00:31:31 +0100

    add styled from pages

--- src/pages/MoviesDetails/MovieDetal.js
+++ src/pages/MoviesDetails/MovieDetal.js
@@ -1,8 +1,9 @@
-import css from "./MovieDetal.styled.css"
+
 import { fetchMovieDetal } from "filmApi/filmApi";
 import { defaultImg } from "helpers/defaultFoto";
 import { useState, useEffect } from "react"
 import { Link, Outlet, useParams, useLocation } from "react-router-dom"
+import css from './MovieDetal.module.css'
 
 export const MovieDetal = () => {
   const [data, setData] = useState([]);
@@ -30,38 +31,37 @@ export const MovieDetal = () => {
 
   return (
     <>
-      <Link className={css.backLink} to={location?.state?.from ?? '/'}>Go back</Link>
+      <Link className={css['back-link']} to={location?.state?.from ?? '/'}>Go back</Link>
 
       <div className={css.wrapper}>
-        <div>
-          <img src={poster_path
-            ? `https://image.tmdb.org/t/p/w500${poster_path}` : defaultImg} alt={title} />
+        <div className={css['img-wrapper']}>
+          <img className={css.img} src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : defaultImg} alt={title} />
         </div>
 
-        <div>
-          <h2>{title} {release_data}</h2>
-          <p>Use score: <span>{vote_average}</span> </p>
-          <h3>Overwiew</h3>
+        <div className={css['text-wrapper']}>
+          <h2 className={css.title}>{title} {release_data}</h2>
+          <p className={css.text}>Use score: <span>{vote_average}</span> </p>
+          <h3 className={css['sub-title']}>Overwiew</h3>
           <p>{overview}</p>
           <h3>Genre</h3>
-          <p>{genres.map(({ name }) => name).join(', ')} </p>
-          <h3>Additional information</h3>
+          <p className={css.text}>{genres.map(({ name }) => name).join(', ')} </p>
+          <h3 className={css['sub-title']}>Additional information</h3>
           <ul>
             <li>
-              <Link state={{ from: location?.state?.from }} to={`cast`}>
+              <Link className={css['page-link']} state={{ from: location?.state?.from }} to={`cast`}>
                 Cast
-              </Link >
+              </Link>
             </li>
             <li>
-              <Link state={{ from: location?.state?.from }} to={`reviews`} >Reviews</Link>
+              <Link className={css['page-link']} state={{ from: location?.state?.from }} to={`reviews`} >Reviews</Link>
             </li>
           </ul>
           <Outlet />
         </div>
       </div>
     </>
-
-
-
   )
 }
